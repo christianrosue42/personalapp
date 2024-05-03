dd6e9664323d91d5352d9d0c1c53473f7ec9e545
chrosue <christian.rosue@docc.techstarter.de>
2024-05-03 16:11:28 +0200

    update terraform script to store default state in previously created s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "robohub-terraform-state"
+    key    = "terraformstate"
+    region = "eu-central-1"
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
