9f6637f2e93e8dd40b685aa32dc14164db26d1c0
chrosue <christian.rosue@docc.techstarter.de>
2024-05-04 19:37:35 +0200

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,8 +115,8 @@ resource "aws_security_group_rule" "web_sg_ingress" {
 
 resource "aws_security_group_rule" "web_sg_egress" {
   type              = "egress"
-  from_port         = 0
-  to_port           = 0
+  from_port         = 443
+  to_port           = 443
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.web_sg.id
