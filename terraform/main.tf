db951094b5f84613e660cce8d3c9f55d4328f6ba
chrosue <christian.rosue@docc.techstarter.de>
2024-05-04 17:51:41 +0200

    add second nat gateway to private subnet 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,20 +38,19 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# create nat gateway for backend in private subnets
+# create nat gateway for backend in private subnet 1
 
 resource "aws_eip" "nat_eip" {
-  # vpc = true # deprecated
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnet_1.id
 }
 
-# create route table for private subnets
+# create route table for private subnet 1
 
-resource "aws_route_table" "private_route_table" {
+resource "aws_route_table" "private_route_table_1" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -60,16 +59,39 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-# associate route table with private subnets
+# associate route table with private subnet 1
 
 resource "aws_route_table_association" "private_subnet_1_association" {
   subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.private_route_table_1.id
 }
+
+# create nat gateway for backend in private subnet 2
+
+resource "aws_eip" "nat_eip_2" {
+}
+
+resource "aws_nat_gateway" "nat_2" {
+  allocation_id = aws_eip.nat_eip_2.id
+  subnet_id     = aws_subnet.public_subnet_2.id
+}
+
+# create route table for private subnet 2
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_2.id
+  }
+}
+
+# associate route table with private subnet 2
 
 resource "aws_route_table_association" "private_subnet_2_association" {
   subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.private_route_table_2.id
 }
 
 resource "aws_security_group" "web_sg" {
