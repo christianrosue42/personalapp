96d6f8df8b69ee94530fbf7627c2c1b06feaa041
chrosue <christian.rosue@docc.techstarter.de>
2024-05-05 15:19:49 +0200

    fix apiControler not found issue - rename the file apiControler.js to apiController.js (typo)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const apiController = require('./controller/apiController');
+const apiController = require('./controler/apiControler');
 
 const app = express();
 
