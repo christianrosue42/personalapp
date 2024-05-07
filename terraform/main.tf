0a2acf84a0a243901e84c4e86d3e669bbc3578cf
chrosue <christian.rosue@docc.techstarter.de>
2024-05-07 12:23:50 +0200

    add backend server port to fetch request and fix listener port unreachable error by adding second security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,7 +167,7 @@ resource "aws_lb" "alb" {
   name               = "robohub-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_sg.id]
+  security_groups    = [aws_security_group.web_sg.id, aws_security_group.app_sg.id]
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 }
 
